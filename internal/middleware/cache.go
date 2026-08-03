2412d0cd1a7b0b28b474ea05613f9cd0ffc277bd
IshanHunt77 <ishanraghuvanshi046@gmail.com>
2026-08-04 00:54:53 +0530

    added cache buffer and writer

--- internal/middleware/cache.go
+++ internal/middleware/cache.go
@@ -1,24 +1,42 @@
 package middleware
 
 import (
+	"bytes"
 	"log/slog"
 	"net/http"
 
 	"github.com/IshanHunt77/llm-gateway/internal/cache"
 )
 
+
+type wrapper struct {
+	 http.ResponseWriter
+	buff bytes.Buffer
+}
+
+func (rw *wrapper) Write(b []byte) (int,error) {
+					rw.buff.Write(b)
+					val,err:=rw.ResponseWriter.Write(b)
+					return val,err
+				}
+
 func Caching(c *cache.Cache) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler{
 		return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request){
 			key := r.URL.Path
-			_,ok:=c.Read(key)
+			val,ok:=c.Read(key)
 			if !ok {
+				wrap := wrapper{ResponseWriter: w}
+				next.ServeHTTP(&wrap,r)
+				c.Upsert(key,wrap.buff.String())
 				slog.Info("miss")
 			}else{
-				slog.Info("hit")	
+				w.Write([]byte(val))
+				slog.Info("Hit")
+				return
+				
 			}
 			
-			next.ServeHTTP(w,r)
 		})
 	}
 }
